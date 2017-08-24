ad82d79e96b6b09b3784489fe09f2d1eca4cd980
Tristan Schmitt <trschmitt50@live.com>
2017-08-23 20:41:02 -0400

    Quit option added

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ const {app, Menu, Tray} = require('electron')
 
 let tray = null
 app.on('ready', () => {
-  tray = new Tray('images/tray_icon_black.png')
+  tray = new Tray('images/tray_icon_purple.png')
   const contextMenu = Menu.buildFromTemplate([
     {label: 'Item1', type: 'radio'},
     {label: 'Item2', type: 'radio'},
     {label: 'Item3', type: 'radio', checked: true},
-    {label: 'Item4', type: 'radio'}
+    {label: 'Item4', type: 'radio'},
+    {label: 'Quit', role: 'quit'}
   ])
   tray.setToolTip('This is my application.')
   tray.setContextMenu(contextMenu)
